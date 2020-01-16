caf829574ac1c238619ce55c490c56ca5a100f9e
Martin Laws <martinblaws@gmail.com>
2020-01-16 07:53:20 -0500

    Adding 0a and 0b code demos

--- 2_promises.js
+++ 2_promises.js
@@ -1,35 +1,70 @@
+// Let's solve it again using promises!
+
 const fs = require('fs').promises;
 
-const p = fs.readFile('data/p1.txt', 'utf8'); // => Promise 1
-// console.log('p first: ', p); // p.state => pending
+// console.log(fs);
 
-const finalDoc = [];
+// Reads all of the 4 files and returns a promise
+const readFourAgileFiles = function(done) { 
+  let finalDoc = [];
 
-p
-  .then((data) => {
-    // console.log('p after: ', p); // p.state => fulfilled
-    // console.log('DATA: ', data);
-    finalDoc.push(data);
-    return fs.readFile('data/p2.txt', 'utf8') // => Promise 2
-  })
-  .then((data) => {
-    finalDoc.push(data);
-    return fs.readFile('data/p3.txt', 'utf8') // => Promise 3
-  })
-  .then((data) => {
-    finalDoc.push(data);
-    return fs.readFile('data/p4.txt', 'utf8') // => Promise 4
-  })
-  .then((data) => {
-    finalDoc.push(data);
-    console.log('in final .then, finalDoc = ', finalDoc);
+  const p1 = fs.readFile("data/p1.txt", "utf8");
+  
+  return p1
+    .then((data1) => {
+      console.log('data1 is: ', data1);
+      finalDoc.push(data1);
+      return fs.readFile("data/p2.txt", "utf8");
+    })
+    .then((data2) => {
+      console.log('data2 is: ', data2); // p2 text
+      finalDoc.push(data2); 
+      return fs.readFile("data/p3.txt", "utf8");
+    })
+    .then((data3) => {
+      console.log('data3 is: ', data3); // p3 text
+      finalDoc.push(data3); 
+      return fs.readFile("data/p4.txt", "utf8");
+    })
+    .then((data4) => {
+      console.log('data4 is: ', data4); // p4 text
+      finalDoc.push(data4); 
+      // console.log('DONE, finalDoc is: ', finalDoc);
+      return finalDoc;
+      // return finalDoc;
+    });
+}
+
+// can't return a result synchronously.
+// const result = readFourAgileFiles();
+// console.log(result);
+
+// readFourAgileFiles((result) => {
+//   console.log('result: ', result);
+// });
+
+readFourAgileFiles()
+  .then((result) => {
+    console.log('result: ', result);
   })
   .catch((err) => {
-    console.log('in catch: ', finalDoc);
-    console.log('Error: ', err.message);
+    console.log('OH NO!', err);
   });
-  
-console.log('last line: ', finalDoc);
 
 
+// wrong (callback hell-y approach)
+// p1.then((data) => {
+//   console.log(data);
+//   console.log(p1);
+//   finalDoc.push(data);
+//   const p2 = fs.readFile("data/p2.txt", "utf8");
+//   p2.then((data) => {
+//     finalDoc.push(data);
+//     const p3 = fs.readFile("data/p2.txt", "utf8");
+//     p3.then((data) => {
 
+//     });
+//   }); 
+// });
+
+// console.log(p1);
---
+++ agenda.md
@@ -0,0 +1,52 @@
+# Promises
+=====
+
+## 0. Review on Callbacks
+
+What? 
+
+```js
+a()
+b()
+```
+
+- Function that's passed into another function as a variable/arg
+- The callback fn is called back when a specific action has been done
+
+Why? 
+
+- JS does not like to be "blocked" by a()
+- Concurrency Models (JS has a specific one. Event based. Event Loop)
+
+## 1. Expectation Setting [5m]
+
+Promises!! =/
+
+## 2. Review Callback Hell [2m]
+
+Let's look at the Profile Generator assignment.
+
+Nested callbacks happen in JS 
+Because one relies on the other. 
+
+## 3. Promise-based Solution (Preview) [10m]
+
+- Callbacks are still used with Promises, just not the crazy stairecase/waterfall hell.
+
+- As a preview of things to come, let's look at the solution for it.
+- Note the separate npm module
+
+## 4. Wait, what are Promises? [15m]
+
+- Analogy
+
+## 5. Practice Problem with FS (Callbacks first) [20m] 
+
+Problem: 
+Read the data files ** in order **, creating one final string as a result
+
+## 6. Now with Promises [20m]
+
+## 7. Stretch: Create Promises [20m]
+
+## 8. Stretch: Promise.all and Promise.race (introduction) [5m]
